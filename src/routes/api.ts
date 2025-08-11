31f1c21a4d1b778139613221660d3b8ed9076795
Tyrone <tyrone@clearmindai.com>
2025-08-11 18:00:03 +0800

    api new import

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,6 @@
 import express,{Request, Response, NextFunction} from "express";
 const router = express.Router();
-import {addStaff} from "../controllers/StaffController";
+import {addStaff} from "../controllers/sample";
 
 router.post("/addStaff", addStaff);
 
